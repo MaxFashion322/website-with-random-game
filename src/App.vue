6b75f8c48803419c0725d3ed87005f61b95840f8
Maxim_Golostsuk <mgoloshuk@gmail.com>
2025-10-12 18:10:27 +0300

    Added visual effects

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <script setup>
+function playClickSound(){
+  const audio = new Audio("/public/sound/mouse_click.wav");
+  audio.play();
+}
+
+
 </script>
 
 <template>
-  <div class="bg"></div>
+  <div class="wrapper">
+    <div class="inner_column">
+      <div class="header">
+        <div class="dvdGif"></div>
+        <h1>neveroyatno original'no</h1>
+      </div>
+      <div class="retro-container">
+        <button class="retro-btn" @click="playClickSound"> Click here</button>
+      </div>
+      <div class="bgGif"></div>
+    </div>
+  </div>
+
 
 </template>
 
