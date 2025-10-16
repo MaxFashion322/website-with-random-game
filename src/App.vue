529b03e8fe2af805202f3702cb2b8aaa76a2006a
Maxim_Golostsuk <mgoloshuk@gmail.com>
2025-10-16 18:27:46 +0300

    The basic GUI is ready. I'll stick to this style.

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,39 @@
 <script setup>
-function playClickSound(){
-  const audio = new Audio("/sound/mouse_click.wav");
-  audio.play();
-}
+// function playClickSound(){
+//   const audio = new Audio("/sound/mouse_click.wav");
+//   audio.play();
+// }
+const playClickSound = () => new Audio("/sound/mouse_click.wav").play()
+
+
+
+
 </script>
 
 <template>
   <div id="wrapper">
-    <div id="main_column">
-      <div id="library" class="menu_bar">
-        <span class="menu_item">Library</span>
-      </div>
-      <div class="retro-container">
-        <button class="retro-btn" @click="playClickSound"> Click here</button>
+    <div id="main">
+      <div class="menu_bar">
+        <div class="menu_item" @click="playClickSound">Library </div>
+        <div class="menu_item" @click="playClickSound">List of Games </div>
       </div>
 
-      <div id="bgGif"></div>
+      <h1 class="main_title">BROTHER YOU CAN'T DECIDE, WHICH GAME TO PLAY???🎮
+        <br>
+        <br>well...
+        <br>
+        <br> press the button, maybe it helps 🤔
+      </h1>
+
+      <div class="retro_container">
+        <button class="retro_btn" @click="playClickSound"> Click here</button>
+      </div>
+
+      <div id="bg_gif"></div>
     </div>
   </div>
 
-
+  <div id="bg"></div>
 </template>
 
 <style scoped> </style>
